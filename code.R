0e2747366effe5ec623b7271155c43a038ebeba0
vad5642 <vad5642@gmail.com>
2015-08-18 22:58:23 +0200

    +

--- code.R
+++ code.R
@@ -13,5 +13,44 @@ download_data <- function()
 
 
 #download_data()
-stormData <- read.csv("data.csv")
+#stormData <- read.csv("data.csv")
+
+library(plyr)
+
+# translate all letters to lowercase
+stormData$EVTYPE <- tolower(stormData$EVTYPE)
+# replace all punct. characters with a space
+stormData$EVTYPE <- gsub("[[:blank:][:punct:]+]", " ", stormData$EVTYPE)
+length(unique(stormData$EVTYPE))
+
+harm <- ddply(stormData, .(EVTYPE), summarize,
+                    killed = sum(FATALITIES),
+                    injuried = sum(INJURIES),
+                    property = .(PROPDMG),
+                    property_exp = tolower(PROPDMGEXP),
+                    crop = .(CROPDMG),
+                    crop_exp = tolower(CROPDMGEXP))
+
+
+              
+
+exp_transform <- function(e) {
+    # h -> hundred, k -> thousand, m -> million, b -> billion
+    if (e %in% c('h', 'H'))
+        return(2)
+    else if (e %in% c('k', 'K'))
+        return(3)
+    else if (e %in% c('m', 'M'))
+        return(6)
+    else if (e %in% c('b', 'B'))
+        return(9)
+    else if (!is.na(as.numeric(e))) # if a digit
+        return(as.numeric(e))
+    else if (e %in% c('', '-', '?', '+'))
+        return(0)
+    else {
+        stop("Invalid exponent value.")
+    }
+}
+
 
